a11c39f9e92fc30422902f7abe0eadf933612dd6
Andrea Cannavo <andrea.cannavo.7@gmail.com>
2024-11-12 22:57:58 +0100

    [CHANGE] team headers dimensions, removev search from header

--- src/components/features/Draft/draft-header.tsx
+++ src/components/features/Draft/draft-header.tsx
@@ -1,5 +1,3 @@
-import { Search } from "lucide-react";
-
 export default function DraftHeader({ 
     blueTeamName = 'Blue', 
     redTeamName = 'Red',
@@ -7,22 +5,18 @@ export default function DraftHeader({
 }: {blueTeamName: string, redTeamName: string, timer: number | null}) {
 
     return(
-        <div className="flex justify-between items-center mb-4">
-        <div className="bg-blue-700 text-white px-4 py-2 w-48 text-center">
+      <div className="flex justify-between items-center mb-4">
+        <div className="bg-blue-700 text-white px-4 py-2 w-[25%] text-center text-2xl font-bold">
           {blueTeamName}
         </div>
-        <div className="flex space-x-2">
-          <div className="text-white text-2xl">{timer ? timer : ''}</div>
+
+        <div className="flex justify-center items-center w-[30%]">
+          <div className="text-white text-4xl font-bold">
+            {timer ? timer : ''}
+          </div>
         </div>
-        <div className="relative w-64">
-          <Search className="absolute left-2 top-2 text-gray-400" size={20} />
-          <input
-            type="text"
-            placeholder="Search champion..."
-            className="w-full bg-gray-800 text-white pl-10 pr-4 py-2 rounded"
-          />
-        </div>
-        <div className="bg-red-700 text-white px-4 py-2 w-48 text-center">
+
+        <div className="bg-red-700 text-white px-4 py-2 w-[25%] text-center text-2xl font-bold">
           {redTeamName}
         </div>
       </div>
