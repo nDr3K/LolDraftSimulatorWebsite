73c80cd7864a178068d563e3cd98cc201a662f1e
Andrea Cannavo <andrea.cannavo.7@gmail.com>
2024-11-21 20:24:40 +0100

    [ADD] margin between selection and grid

--- src/pages/Draft/Draft.tsx
+++ src/pages/Draft/Draft.tsx
@@ -81,7 +81,7 @@ export default function Draft() {
           <DraftTeam team={[null,null,null,null,null]} side='blue' />
         </div>
         <div className="flex-1 flex flex-col overflow-hidden">
-          <div className="flex-shrink-0">
+          <div className="flex-shrink-0 mb-2">
             <DraftSelection onRoleSelect={handleRoleSelect} onSearchChange={handleSearchChange} />
           </div>
           <div className="flex-1 overflow-y-auto">
